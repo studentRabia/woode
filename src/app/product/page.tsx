3222c26828bb8460bd5833dbf163e1073a7d0733
studentRabia <pubgali125.com@gmail.com>
2025-02-03 00:26:28 +0500

    2rd commit

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import FutureProduct from '../components/futureProduct'
-import Products from '../components/products'
 import NewsletterSection from '../components/Newsletter'
 import Shop from '../shop/page'
 
 const page = () => {
   return (
     <div>
- {/* <FutureProduct/>
- <Products/> */}
+
 
  <Shop/>
  <NewsletterSection/>
